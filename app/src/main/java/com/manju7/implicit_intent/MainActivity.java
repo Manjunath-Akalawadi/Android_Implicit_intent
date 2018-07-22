fe2a7358b4f7d6293b225d27214c804a02284f5b
manju <manjuakalawadi@gmail.com>
2018-07-22 21:06:04 +0530

    Initial commit

---
+++ app/src/main/java/com/manju7/implicit_intent/MainActivity.java
@@ -0,0 +1,69 @@
+package com.manju7.implicit_intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonW, buttonE , buttonG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonW = (Button) findViewById(R.id.buttonW);
+        buttonE = (Button) findViewById(R.id.buttonE);
+        buttonG = (Button) findViewById(R.id.buttonG);
+
+        buttonW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri uri = Uri.parse("smsto:"+"9901536007");
+                Intent i = new Intent(Intent.ACTION_SENDTO,uri);
+                i.setPackage("com.whatsapp");
+                startActivity(i);
+            }
+        });
+
+        buttonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+
+        buttonG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://www.google.com"));
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+    private void sendMail(){
+
+         String to="manjuakalawadi@gmail.com";
+         String sub="subject";
+         String message="message";
+
+         Intent intent = new Intent(Intent.ACTION_SEND);
+         intent.putExtra(Intent.EXTRA_EMAIL,"manjuakalawadi@gmail.com");
+         intent.putExtra(Intent.EXTRA_SUBJECT,"Subject");
+         intent.putExtra(Intent.EXTRA_TEXT,"message");
+
+         intent.setType("message/rfc882");
+         startActivity(Intent.createChooser(intent,"Choose an email"));
+
+    }
+}
